645b1a542bc97453734556e989c5e8eca3ae55eb
lhb <2710575220@qq.com>
2026-01-13 14:40:22 +0800

    1.0.1-version

--- sql/auth.sql
+++ sql/auth.sql
@@ -1,82 +1,91 @@
 /*
 Navicat MySQL Data Transfer
-
 Source Server         : mysql80
 Source Server Version : 80036
 Source Host           : localhost:3306
 Source Database       : auth
-
 Target Server Type    : MYSQL
 Target Server Version : 80036
-File Encoding         : 65001
-
+File Encoding         : UTF-8
 Date: 2026-01-12 14:34:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
+-- 创建数据库
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS `auth`
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE `auth`;
+
+-- ----------------------------
 -- Table structure for `auth_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `auth_user`;
 CREATE TABLE `auth_user` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '???',
-  `username` varchar(50) NOT NULL COMMENT '?û??',
-  `password` varchar(255) NOT NULL COMMENT '???루???ܺ',
-  `email` varchar(100) DEFAULT NULL COMMENT '???',
-  `role` varchar(50) NOT NULL COMMENT '??ɫ',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_username` (`username`),
-  UNIQUE KEY `uk_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='?û??';
+                             `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                             `username` varchar(50) NOT NULL COMMENT '用户名',
+                             `password` varchar(255) NOT NULL COMMENT '密码（加密后）',
+                             `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+                             `role` varchar(50) NOT NULL COMMENT '角色',
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `uk_username` (`username`),
+                             UNIQUE KEY `uk_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
 
 -- ----------------------------
 -- Records of auth_user
 -- ----------------------------
-INSERT INTO `auth_user` VALUES ('1', 'user_1', '$2a$10$3t3UIbkwtwsnzqmrt7/a6uoAvloR1DN52cNKFE1C7vMCPJwr6Upu.', 'user_1@example.com', 'COMMON');
-INSERT INTO `auth_user` VALUES ('2', 'editor_1', '$2a$10$nl.bKP9c9cqQOO2h9m0Iruq8KEr7Gg2jDQIBdZRB98Lo9TEzVnX8i', 'editor_1@example.com', 'EDITOR');
-INSERT INTO `auth_user` VALUES ('3', 'adm_1', '$2a$10$oRtTdHvnGQHuWzbQl4kcp.Vbc7quLpfZ28EL5R4inqr0IGVmtli2S', 'adm_1@example.com', 'ADMIN');
+INSERT INTO `auth_user` VALUES
+                            (1, 'user_1', '$2a$10$3t3UIbkwtwsnzqmrt7/a6uoAvloR1DN52cNKFE1C7vMCPJwr6Upu.', 'user_1@example.com', 'COMMON'),
+                            (2, 'editor_1', '$2a$10$nl.bKP9c9cqQOO2h9m0Iruq8KEr7Gg2jDQIBdZRB98Lo9TEzVnX8i', 'editor_1@example.com', 'EDITOR'),
+                            (3, 'adm_1', '$2a$10$oRtTdHvnGQHuWzbQl4kcp.Vbc7quLpfZ28EL5R4inqr0IGVmtli2S', 'adm_1@example.com', 'ADMIN');
 
 -- ----------------------------
 -- Table structure for `github_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `github_user`;
 CREATE TABLE `github_user` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '???',
-  `github_id` bigint NOT NULL COMMENT 'GitHub ?û?ID',
-  `username` varchar(50) NOT NULL COMMENT '?û??',
-  `email` varchar(100) DEFAULT NULL COMMENT '???',
-  `role` varchar(50) NOT NULL COMMENT '??ɫ',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_github_id` (`github_id`),
-  UNIQUE KEY `uk_username` (`username`),
-  UNIQUE KEY `uk_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='GitHub ?û??';
+                               `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                               `github_id` bigint NOT NULL COMMENT 'GitHub 用户ID',
+                               `username` varchar(50) NOT NULL COMMENT '用户名',
+                               `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+                               `role` varchar(50) NOT NULL COMMENT '角色',
+                               PRIMARY KEY (`id`),
+                               UNIQUE KEY `uk_github_id` (`github_id`),
+                               UNIQUE KEY `uk_username` (`username`),
+                               UNIQUE KEY `uk_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='GitHub 用户表';
 
 -- ----------------------------
 -- Records of github_user
 -- ----------------------------
-INSERT INTO `github_user` VALUES ('1', '170590674', 'lhbchampion', null, 'EDITOR');
+INSERT INTO `github_user` VALUES
+    (1, 170590674, 'lhbchampion', NULL, 'EDITOR');
 
 -- ----------------------------
 -- Table structure for `ldap_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `ldap_user`;
 CREATE TABLE `ldap_user` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `username` varchar(50) NOT NULL COMMENT 'LDAP 登录名',
-  `name` varchar(100) DEFAULT NULL COMMENT '显示名',
-  `password` varchar(255) NOT NULL COMMENT '密码（加密后）',
-  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
-  `role` varchar(50) NOT NULL COMMENT '角色',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_username` (`username`),
-  UNIQUE KEY `uk_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='LDAP 用户表';
+                             `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                             `username` varchar(50) NOT NULL COMMENT 'LDAP 登录名',
+                             `name` varchar(100) DEFAULT NULL COMMENT '显示名',
+                             `password` varchar(255) NOT NULL COMMENT '密码（加密后）',
+                             `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+                             `role` varchar(50) NOT NULL COMMENT '角色',
+                             PRIMARY KEY (`id`),
+                             UNIQUE KEY `uk_username` (`username`),
+                             UNIQUE KEY `uk_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='LDAP 用户表';
 
 -- ----------------------------
 -- Records of ldap_user
 -- ----------------------------
-INSERT INTO `ldap_user` VALUES ('1', 'ldap_user_1', 'LDAP 普通用户', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_user_1@example.com', 'COMMON');
-INSERT INTO `ldap_user` VALUES ('2', 'ldap_editor_1', 'LDAP 编辑员', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_editor_1@example.com', 'EDITOR');
-INSERT INTO `ldap_user` VALUES ('3', 'ldap_adm_1', 'LDAP 管理员', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_adm_1@example.com', 'ADMIN');
+INSERT INTO `ldap_user` VALUES
+                            (1, 'ldap_user_1', 'LDAP 普通用户', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_user_1@example.com', 'COMMON'),
+                            (2, 'ldap_editor_1', 'LDAP 编辑员', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_editor_1@example.com', 'EDITOR'),
+                            (3, 'ldap_adm_1', 'LDAP 管理员', '0f51fa2a-74d7-49ad-ab42-26f1c5dfe07a', 'ldap_adm_1@example.com', 'ADMIN');
--- sql/product.sql
+++ sql/product.sql
@@ -1,132 +1,139 @@
 /*
 Navicat MySQL Data Transfer
-
 Source Server         : mysql80
 Source Server Version : 80036
 Source Host           : localhost:3306
 Source Database       : product
-
 Target Server Type    : MYSQL
 Target Server Version : 80036
-File Encoding         : 65001
-
+File Encoding         : UTF-8
 Date: 2026-01-12 14:34:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
+-- 创建数据库
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS `product`
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE `product`;
+
+-- ----------------------------
 -- Table structure for `product`
 -- ----------------------------
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '??ƷID',
-  `name` varchar(255) NOT NULL COMMENT '??Ʒ?',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='??Ʒ?';
+                           `id` bigint NOT NULL AUTO_INCREMENT COMMENT '产品ID',
+                           `name` varchar(255) NOT NULL COMMENT '产品名',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='产品表';
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
-INSERT INTO `product` VALUES ('1', '产品1');
-INSERT INTO `product` VALUES ('2', '产品2');
-INSERT INTO `product` VALUES ('3', '产品3');
-INSERT INTO `product` VALUES ('4', '产品4');
-INSERT INTO `product` VALUES ('5', '产品5');
-INSERT INTO `product` VALUES ('6', '产品6');
-INSERT INTO `product` VALUES ('7', '产品7');
-INSERT INTO `product` VALUES ('8', '产品8');
-INSERT INTO `product` VALUES ('9', '产品9');
-INSERT INTO `product` VALUES ('10', '产品10');
-INSERT INTO `product` VALUES ('11', '产品11');
-INSERT INTO `product` VALUES ('12', '产品12');
-INSERT INTO `product` VALUES ('13', '产品13');
-INSERT INTO `product` VALUES ('14', '产品14');
-INSERT INTO `product` VALUES ('15', '产品15');
-INSERT INTO `product` VALUES ('16', '产品16');
-INSERT INTO `product` VALUES ('17', '产品17');
-INSERT INTO `product` VALUES ('18', '产品18');
-INSERT INTO `product` VALUES ('19', '产品19');
-INSERT INTO `product` VALUES ('20', '产品20');
-INSERT INTO `product` VALUES ('21', '产品21');
-INSERT INTO `product` VALUES ('22', '产品22');
-INSERT INTO `product` VALUES ('23', '产品23');
-INSERT INTO `product` VALUES ('24', '产品24');
-INSERT INTO `product` VALUES ('25', '产品25');
-INSERT INTO `product` VALUES ('26', '产品26');
-INSERT INTO `product` VALUES ('27', '产品27');
-INSERT INTO `product` VALUES ('28', '产品28');
-INSERT INTO `product` VALUES ('29', '产品29');
-INSERT INTO `product` VALUES ('30', '产品30');
-INSERT INTO `product` VALUES ('31', '产品31');
-INSERT INTO `product` VALUES ('32', '产品32');
-INSERT INTO `product` VALUES ('33', '产品33');
-INSERT INTO `product` VALUES ('34', '产品34');
-INSERT INTO `product` VALUES ('35', '产品35');
-INSERT INTO `product` VALUES ('36', '产品36');
-INSERT INTO `product` VALUES ('37', '产品37');
-INSERT INTO `product` VALUES ('38', '产品38');
-INSERT INTO `product` VALUES ('39', '产品39');
-INSERT INTO `product` VALUES ('40', '产品40');
-INSERT INTO `product` VALUES ('41', '产品41');
-INSERT INTO `product` VALUES ('42', '产品42');
-INSERT INTO `product` VALUES ('43', '产品43');
-INSERT INTO `product` VALUES ('44', '产品44');
-INSERT INTO `product` VALUES ('45', '产品45');
-INSERT INTO `product` VALUES ('46', '产品46');
-INSERT INTO `product` VALUES ('47', '产品47');
-INSERT INTO `product` VALUES ('48', '产品48');
-INSERT INTO `product` VALUES ('49', '产品49');
-INSERT INTO `product` VALUES ('50', '产品50');
-INSERT INTO `product` VALUES ('51', '产品51');
-INSERT INTO `product` VALUES ('52', '产品52');
-INSERT INTO `product` VALUES ('53', '产品53');
-INSERT INTO `product` VALUES ('54', '产品54');
-INSERT INTO `product` VALUES ('55', '产品55');
-INSERT INTO `product` VALUES ('56', '产品56');
-INSERT INTO `product` VALUES ('57', '产品57');
-INSERT INTO `product` VALUES ('58', '产品58');
-INSERT INTO `product` VALUES ('59', '产品59');
-INSERT INTO `product` VALUES ('60', '产品60');
-INSERT INTO `product` VALUES ('61', '产品61');
-INSERT INTO `product` VALUES ('62', '产品62');
-INSERT INTO `product` VALUES ('63', '产品63');
-INSERT INTO `product` VALUES ('64', '产品64');
-INSERT INTO `product` VALUES ('65', '产品65');
-INSERT INTO `product` VALUES ('66', '产品66');
-INSERT INTO `product` VALUES ('67', '产品67');
-INSERT INTO `product` VALUES ('68', '产品68');
-INSERT INTO `product` VALUES ('69', '产品69');
-INSERT INTO `product` VALUES ('70', '产品70');
-INSERT INTO `product` VALUES ('71', '产品71');
-INSERT INTO `product` VALUES ('72', '产品72');
-INSERT INTO `product` VALUES ('73', '产品73');
-INSERT INTO `product` VALUES ('74', '产品74');
-INSERT INTO `product` VALUES ('75', '产品75');
-INSERT INTO `product` VALUES ('76', '产品76');
-INSERT INTO `product` VALUES ('77', '产品77');
-INSERT INTO `product` VALUES ('78', '产品78');
-INSERT INTO `product` VALUES ('79', '产品79');
-INSERT INTO `product` VALUES ('80', '产品80');
-INSERT INTO `product` VALUES ('81', '产品81');
-INSERT INTO `product` VALUES ('82', '产品82');
-INSERT INTO `product` VALUES ('83', '产品83');
-INSERT INTO `product` VALUES ('84', '产品84');
-INSERT INTO `product` VALUES ('85', '产品85');
-INSERT INTO `product` VALUES ('86', '产品86');
-INSERT INTO `product` VALUES ('87', '产品87');
-INSERT INTO `product` VALUES ('88', '产品88');
-INSERT INTO `product` VALUES ('89', '产品89');
-INSERT INTO `product` VALUES ('90', '产品90');
-INSERT INTO `product` VALUES ('91', '产品91');
-INSERT INTO `product` VALUES ('92', '产品92');
-INSERT INTO `product` VALUES ('93', '产品93');
-INSERT INTO `product` VALUES ('94', '产品94');
-INSERT INTO `product` VALUES ('95', '产品95');
-INSERT INTO `product` VALUES ('96', '产品96');
-INSERT INTO `product` VALUES ('97', '产品97');
-INSERT INTO `product` VALUES ('98', '产品98');
-INSERT INTO `product` VALUES ('99', '产品99');
-INSERT INTO `product` VALUES ('100', '产品100');
-INSERT INTO `product` VALUES ('101', '对大着火状使');
-INSERT INTO `product` VALUES ('102', '商品102');
+INSERT INTO `product` (`id`, `name`) VALUES
+                                         (1, '产品1'),
+                                         (2, '产品2'),
+                                         (3, '产品3'),
+                                         (4, '产品4'),
+                                         (5, '产品5'),
+                                         (6, '产品6'),
+                                         (7, '产品7'),
+                                         (8, '产品8'),
+                                         (9, '产品9'),
+                                         (10, '产品10'),
+                                         (11, '产品11'),
+                                         (12, '产品12'),
+                                         (13, '产品13'),
+                                         (14, '产品14'),
+                                         (15, '产品15'),
+                                         (16, '产品16'),
+                                         (17, '产品17'),
+                                         (18, '产品18'),
+                                         (19, '产品19'),
+                                         (20, '产品20'),
+                                         (21, '产品21'),
+                                         (22, '产品22'),
+                                         (23, '产品23'),
+                                         (24, '产品24'),
+                                         (25, '产品25'),
+                                         (26, '产品26'),
+                                         (27, '产品27'),
+                                         (28, '产品28'),
+                                         (29, '产品29'),
+                                         (30, '产品30'),
+                                         (31, '产品31'),
+                                         (32, '产品32'),
+                                         (33, '产品33'),
+                                         (34, '产品34'),
+                                         (35, '产品35'),
+                                         (36, '产品36'),
+                                         (37, '产品37'),
+                                         (38, '产品38'),
+                                         (39, '产品39'),
+                                         (40, '产品40'),
+                                         (41, '产品41'),
+                                         (42, '产品42'),
+                                         (43, '产品43'),
+                                         (44, '产品44'),
+                                         (45, '产品45'),
+                                         (46, '产品46'),
+                                         (47, '产品47'),
+                                         (48, '产品48'),
+                                         (49, '产品49'),
+                                         (50, '产品50'),
+                                         (51, '产品51'),
+                                         (52, '产品52'),
+                                         (53, '产品53'),
+                                         (54, '产品54'),
+                                         (55, '产品55'),
+                                         (56, '产品56'),
+                                         (57, '产品57'),
+                                         (58, '产品58'),
+                                         (59, '产品59'),
+                                         (60, '产品60'),
+                                         (61, '产品61'),
+                                         (62, '产品62'),
+                                         (63, '产品63'),
+                                         (64, '产品64'),
+                                         (65, '产品65'),
+                                         (66, '产品66'),
+                                         (67, '产品67'),
+                                         (68, '产品68'),
+                                         (69, '产品69'),
+                                         (70, '产品70'),
+                                         (71, '产品71'),
+                                         (72, '产品72'),
+                                         (73, '产品73'),
+                                         (74, '产品74'),
+                                         (75, '产品75'),
+                                         (76, '产品76'),
+                                         (77, '产品77'),
+                                         (78, '产品78'),
+                                         (79, '产品79'),
+                                         (80, '产品80'),
+                                         (81, '产品81'),
+                                         (82, '产品82'),
+                                         (83, '产品83'),
+                                         (84, '产品84'),
+                                         (85, '产品85'),
+                                         (86, '产品86'),
+                                         (87, '产品87'),
+                                         (88, '产品88'),
+                                         (89, '产品89'),
+                                         (90, '产品90'),
+                                         (91, '产品91'),
+                                         (92, '产品92'),
+                                         (93, '产品93'),
+                                         (94, '产品94'),
+                                         (95, '产品95'),
+                                         (96, '产品96'),
+                                         (97, '产品97'),
+                                         (98, '产品98'),
+                                         (99, '产品99'),
+                                         (100, '产品100'),
+                                         (101, '对大着火状使'),
+                                         (102, '商品102');
